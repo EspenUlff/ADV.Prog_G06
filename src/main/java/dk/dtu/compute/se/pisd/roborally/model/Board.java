93bb57ad24baa5fb31200d609e57a20ae504beaf
EspenUlff <91070785+EspenUlff@users.noreply.github.com>
2022-04-20 16:49:27 +0200

    Board notation fixes

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Board.java
@@ -27,6 +27,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import static dk.dtu.compute.se.pisd.roborally.model.Heading.*;
 import static dk.dtu.compute.se.pisd.roborally.model.Phase.INITIALISATION;
 
 /**
@@ -169,53 +170,45 @@ public class Board extends Subject {
         return "Phase: " + getPhase().name() +
                 ", Player: " + getCurrentPlayer().getName() +
                 ", Step: " + getStep();
-
-    /**
-     * Returns the neighbour of the given space of the board in the given heading.
-     * The neighbour is returned only, if it can be reached from the given space
-     * (no walls or obstacles in either of the involved spaces); otherwise,
-     * null will be returned.
-     *
-     * @param space the space for which the neighbour should be computed
-     * @param heading the heading of the neighbour
-     * @return the space in the given direction; null if there is no (reachable) neighbour
-     */
-    public Space getNeighbour(@NotNull Space space, @NotNull Heading heading) {
-        if (space.getWalls().contains(heading)) {
-            return null;
-        }
-        // TODO needs to be implemented based on the actual spaces
-        //      and obstacles and walls placed there. For now it,
-        //      just calculates the next space in the respective
-        //      direction in a cyclic way.
-
-        // XXX an other option (not for now) would be that null represents a hole
-        //     or the edge of the board in which the players can fall
-
-        int x = space.x;
-        int y = space.y;
-        switch (heading) {
-            case SOUTH:
-                y = (y + 1) % height;
-                break;
-            case WEST:
-                x = (x + width - 1) % width;
-                break;
-            case NORTH:
-                y = (y + height - 1) % height;
-                break;
-            case EAST:
-                x = (x + 1) % width;
-                break;
-        }
-        Heading reverse = Heading.values()[(heading.ordinal() + 2)% Heading.values().length];
-        Space result = getSpace(x, y);
-        if (result != null) {
-            if (result.getWalls().contains(reverse)) {
-                return null;
-            }
-        }
-        return result;
     }
 
-}
+        /**
+         * Returns the neighbour of the given space of the board in the given heading.
+         * The neighbour is returned only, if it can be reached from the given space
+         * (no walls or obstacles in either of the involved spaces); otherwise,
+         * null will be returned.
+         *
+         * @param space the space for which the neighbour should be computed
+         * @param heading the heading of the neighbour
+         * @return the space in the given direction; null if there is no (reachable) neighbour
+         */
+        public Space getNeighbour (@NotNull Space space, @NotNull Heading heading){
+            if (space.getWalls().contains(heading)) {
+                return null;
+            }
+            // TODO needs to be implemented based on the actual spaces
+            //      and obstacles and walls placed there. For now it,
+            //      just calculates the next space in the respective
+            //      direction in a cyclic way.
+
+            // XXX an other option (not for now) would be that null represents a hole
+            //     or the edge of the board in which the players can fall
+
+            int x = space.x;
+            int y = space.y;
+            switch (heading) {
+                case SOUTH -> y = (y + 1) % height;
+                case WEST -> x = (x + width - 1) % width;
+                case NORTH -> y = (y + height - 1) % height;
+                case EAST -> x = (x + 1) % width;
+            }
+            Heading reverse = Heading.values()[(heading.ordinal() + 2) % Heading.values().length];
+            Space result = getSpace(x, y);
+            if (result != null) {
+                if (result.getWalls().contains(reverse)) {
+                    return null;
+                }
+            }
+            return result;
+        }
+    }
