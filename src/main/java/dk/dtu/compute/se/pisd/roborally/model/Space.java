1542ef13a22da5d0ac6852298497357ab35d60aa
EspenUlff <91070785+EspenUlff@users.noreply.github.com>
2022-06-09 11:21:00 +0200

    imported NotNull annotations and CheckPoint class to Space class

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Space.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Space.java
@@ -22,8 +22,10 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
+import dk.dtu.compute.se.pisd.roborally.controller.Checkpoint;
 import dk.dtu.compute.se.pisd.roborally.controller.ConveyorBelt;
 import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
